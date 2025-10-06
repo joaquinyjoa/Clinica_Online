a969578bafbc5bfc0f851f5b5fcbeb5203b36fe9
joaquinyjoa <joaquinalfredogreco@gmail.comgit config --global user.email joaquinalfredogreco@gmail.com>
2025-10-06 11:03:51 -0300

    Conexion al hosting de firebase echa

---
+++ clinica/src/app/services/firebase.ts
@@ -0,0 +1,31 @@
+// src/app/services/firebase.service.ts
+import { Injectable } from '@angular/core';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+
+  app;
+  db;
+  auth;
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDHJGMQKOkye1Wz3XNK5CNAVpFBQg2hdqk",
+      authDomain: "clinica-online-6537c.firebaseapp.com",
+      projectId: "clinica-online-6537c",
+      storageBucket: "clinica-online-6537c.appspot.com",
+      messagingSenderId: "13812693037",
+      appId: "1:13812693037:web:bf59a9d38f44ca15d2e0a4",
+      measurementId: "G-NX3ZRXWEWR"
+    };
+
+    this.app = initializeApp(firebaseConfig);
+    this.db = getFirestore(this.app);
+    this.auth = getAuth(this.app);
+  }
+}
